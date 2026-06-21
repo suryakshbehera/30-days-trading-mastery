734fc48aa857cf389092495ea223594f1edfe88a
QVORA <dev@qvor.in>
2026-06-21 13:02:05 +0530

    app installation feature

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'intraday30-v1';
+const ASSETS = ['/index.html', '/day-01.html', '/css/book.css', '/manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy));
+      return res;
+    }).catch(() => cached))
+  );
+});
