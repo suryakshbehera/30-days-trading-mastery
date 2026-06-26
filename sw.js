e3aa409e94bfb4d7289c8c18db7679f548f94812
QVORA <dev@qvor.in>
2026-06-26 08:09:10 +0530

    day4 added

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'intraday30-v4';
-const ASSETS = ['/index.html', '/day-01.html', '/day-02.html', '/day-03.html', '/css/book.css', '/js/notify.js', '/js/daynav.js', '/manifest.json'];
+const CACHE = 'intraday30-v5';
+const ASSETS = ['/index.html', '/day-01.html', '/day-02.html', '/day-03.html', '/day-04.html', '/css/book.css', '/js/notify.js', '/js/daynav.js', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
